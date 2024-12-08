3099233ae9182c4f10c8a03f5dec79c1298a3726
Omer Abbasi <abbasiomer65@gmail.com>
2024-12-08 12:41:14 -0800

    Fixed2

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
         <nav className='mynav inline'>
           <div className='flex justify-center'>  
             <ul className='mynavUl flex'>
-              <li><a href="/">Home</a></li>
+              <li><a href="#home">Home</a></li>
               <li><a href="#shop">Shop</a></li>
               <li><a href="#about">About</a></li>
               <li><a href="#contact">Contact</a></li>
